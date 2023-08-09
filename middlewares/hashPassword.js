b1eee3c99443b1956fcae0e0b0e3c5ce47842000
John Roniel <60148238+JRoniel@users.noreply.github.com>
2023-08-08 22:37:05 -0300

    a

--- middlewares/hashPassword.js
+++ middlewares/hashPassword.js
@@ -1,18 +1,10 @@
 const bcrypt = require('bcrypt');
 
-const saltRounds = 10;
-
-async function hashPassword(password) {
-  try {
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
-  } catch (error) {
-    console.error('Erro ao criptografar a senha:', error);
-    throw error;
-  }
+function hashPassword(password) {
+    const saltRounds = 10;
+    return bcrypt.hashSync(password, saltRounds);
 }
 
 module.exports = {
-  hashPassword
+    hashPassword
 };
