0fe20624c6f8ecff489b82b0e7d073a11ffb61a7
John Roniel <johnroniel8264@gmail.com>
2024-05-26 13:30:26 -0300

    correções gerais

--- middlewares/Language.js
+++ middlewares/Language.js
@@ -5,14 +5,16 @@
      */
     function getMessage(message) {
         const messagesList = {
-                // MENSAGENS GERAIS
-                NO_ROUTE_ESTABELECED: 'Nenhuma rota estabelecida',
-                INITIALIZE : 'Sistema iniciado com sucesso!',
-                INTERNAL_ERROR: 'Erro interno, tente novamente mais tarde',
-                INVALID_LEVEL: 'Nível de segurança não autorizado',
-                INVALID_NOTIFY: 'Mensagem inválida',
+                // SUCESSO
+                INITIALIZE : 'Sistema iniciado com sucesso!',  
+                MESSAGE_SUCESS: 'Mensagem enviada com sucesso',
+                REGISTER_SUCESS: 'Registro efetuado com sucesso',
 
-                // MENSAGENS AUTH
+                // PERMISSÃO NÃO AUTORIZADA
+                PERMISSION_DENIED: 'Permissão não autorizada',
+                LEVEL_DENIED: 'Nível de segurança não autorizado',
+
+                // ERRO
                 LOGIN_ERROR: 'Erro ao tentar fazer login',
                 USER_NOT_FOUND: 'Usuário não encontrado',
                 INVALID_PASSWORD_REGISTER: 'Senha não permitida, use letras e números, minimo 8 caracteres',
@@ -20,6 +22,15 @@
                 INVALID_PASSWORD: 'Senha inválida, 8 caracteres, letras e números',
                 INVALID_NAME: 'Nome inválido, somente letras são permitidas',
                 INVALID_EMAIL: 'Email inválido, tente novamente email',
+                NO_ROUTE_ESTABELECED: 'Nenhuma rota estabelecida',
+                INTERNAL_ERROR: 'Erro interno, memsagem:',
+                INVALID_LEVEL: 'Nível de segurança não é valido',
+                INVALID_MESSAGE: 'Mensagem inválida',
+
+                // NÃO ENCONTRADO
+                NO_DATA: 'Nenhuma dado encontrado',
+                ERROR_404: 'Rota não encontrada',
+
         };
 
         return messagesList[message] || 'Mensagem nao encontrada';
