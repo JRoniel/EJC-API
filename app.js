d1dff4c28fe938485c373b9b82cc08f2779d14cc
John Roniel <johnroniel8264@gmail.com>
2024-05-26 13:29:00 -0300

    Adicionado tratamento de rota não encontrada. 404

--- app.js
+++ app.js
@@ -25,18 +25,25 @@ consign()
   .into(app);
 
 app.get("/", async (req, res) => {
-  try {
-    const message = Language.getMessage('NO_ROUTE_ESTABELECED');
-    res.status(200).json(message);
-  } catch (error) {
-    res.status(500).send('Something broke!');
-  }
+  const message = Language.getMessage('NO_ROUTE_ESTABELECED');
+  res.status(200).json(message);
 });
 
+// Capturando rotas não estabelecidas (404)
+app.use((req, res, next) => {try {
+  const message = Language.getMessage('ERROR_404');
+  res.status(200).json(message);
+} catch (error) {
+  res.status(500).send(Language.getMessage('INTERNAL_ERROR' + error));
+}
+});
+
+// Tratamento de erros gerais
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send(Language.getMessage('INTERNAL_ERROR' + error));
 });
+
 
 // Inicializa a aplicacao
 const port = process.env.PORT || 3000;
